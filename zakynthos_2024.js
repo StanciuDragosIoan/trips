250b750f50ccfa2f2345cdd2b918a8342f773058
Stanciu Dragoș Ioan <stanciudragosioan92@gmail.com>
2024-07-26 12:07:52 +0300

    Create zakynthos_2024.js

---
+++ zakynthos_2024.js
@@ -0,0 +1,42 @@
+/*
+
+
+Dafni beach (plaja cu testoase/zona protejata)
+    plaja super draguta, google maps arata 41 min cu masina
+    https://www.tripadvisor.com/Attraction_Review-g189462-d5003190-Reviews-Dafni_Beach-Zakynthos_Ionian_Islands.html
+
+
+
+
+Korakonisi (insula/plaja)
+    34 min cu masina
+    https://www.tripadvisor.com/Attraction_Review-g189462-d11772651-Reviews-Korakonissi-Zakynthos_Ionian_Islands.html
+
+
+
+St.Nicholas
+    41 min cu masina
+    https://www.tripadvisor.com/Attraction_Review-g776001-d4931616-Reviews-St_Nicholas_Beach-Vasilikos_Zakynthos_Ionian_Islands.html
+
+
+
+Gerakas (plaja)
+    43 min cu masina    
+    https://www.tripadvisor.com/Attraction_Review-g776001-d4885489-Reviews-Gerakas_Beach-Vasilikos_Zakynthos_Ionian_Islands.html
+
+
+
+Cross Tavern Zakynthos (loc doar de mancat)
+    42 min cu masina 
+    https://www.tripadvisor.com/Restaurant_Review-g2399635-d1788349-Reviews-Cross_Tavern-Kampi_Zakynthos_Ionian_Islands.html
+
+
+
+Zante Water village (parc acvatic)
+    20 min cu masina
+    https://www.tripadvisor.com/Attraction_Review-g189462-d674872-Reviews-Water_Village-Zakynthos_Ionian_Islands.html
+
+
+Zakynthos Turtle Rescue center (loc unde sunt ingrijite testoase)
+    42 min cu masina
+    https://www.tripadvisor.com/Attraction_Review-g776001-d4916221-Reviews-Zakynthos_Turtle_Rescue_Center-Vasilikos_Zakynthos_Ionian_Islands.html
